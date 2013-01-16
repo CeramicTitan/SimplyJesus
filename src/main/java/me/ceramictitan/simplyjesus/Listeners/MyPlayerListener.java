c610a6ecee93bfa167b78b3715d40366ac830723
CeramicTitan <ceramictitandev@gmail.com>
2013-01-17 00:06:11 +1100

    Remove players from hashset on quit

--- src/main/java/me/ceramictitan/simplyjesus/Listeners/MyPlayerListener.java
+++ src/main/java/me/ceramictitan/simplyjesus/Listeners/MyPlayerListener.java
@@ -1,12 +1,13 @@
 package me.ceramictitan.simplyjesus.Listeners;
 
-import me.ceramictitan.simplyjesus.User.User;
+import main.java.me.ceramictitan.simplyjesus.User.User;
 
 import org.bukkit.EntityEffect;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class MyPlayerListener implements Listener {
 
@@ -14,11 +15,18 @@ public class MyPlayerListener implements Listener {
 	public void onEntityDamage(EntityDamageEvent event) {
 		if (event.getEntity() instanceof Player) {
 			final Player player = ((Player) event.getEntity());
-			;
 			if (User.godlist.contains(player.getName())) {
 				event.setCancelled(true);
 				player.playEffect(EntityEffect.HURT);
 			}
+		}
+	}
+
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		Player p = event.getPlayer();
+		if (User.godlist.contains(p.getName())) {
+			User.godlist.remove(p.getName());
 		}
 	}
 }
